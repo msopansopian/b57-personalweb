1dd6bb517c12085d951db7a94bf1aa0bc09f94a4
msopansopian <m.sopian369@gmail.com>
2024-08-28 14:03:59 +0700

    day 3 - Form Submission

---
+++ assets/js/contact.js
@@ -0,0 +1,43 @@
+function sendEmail(event) {
+  event.preventDefault();
+  // menangkap id di contact.html
+  const name = document.getElementById("inputName").value;
+  const email = document.getElementById("inputEmail").value;
+  const phone = document.getElementById("inputPhoneNumber").value;
+  const subject = document.getElementById("inputSubject").value;
+  const message = document.getElementById("inputMessage").value;
+
+  // pengkondisian untuk memastikan bahwa jangan ada yang kosong
+  if (!name.length) {
+    return alert("name harus di isi");
+  } else if (!email.length) {
+    return alert("email harus di isi");
+  } else if (!phone.length) {
+    return alert("phone number harus di isi");
+  } else if (!subject.length) {
+    return alert("subject harus di isi");
+  } else if (!message.length) {
+    return alert("message harus di isi");
+  }
+
+  //   membuat element a dan memerintahkan saat tag input di isi maka akan di pindahkan ke link gmail untuk kirim email dengan dan membuka tab baru
+
+  let a = document.createElement("a");
+  a.href = `mailto:${email}?subject=${subject}&body=hallo nama saya ${name}, dan nomor HP saya ${phone} ${message}`;
+  a.target = "_blank";
+  a.click();
+
+  //   console.log(`Name : ${name}\nEmail : ${email}\nPhone : ${phone}\nSubject : ${subject}\nMessage : ${message}`);
+
+  let data = {
+    dataName: name,
+    dataEmail: email,
+    dataPhone: phone,
+    dataSubject: subject,
+    dataMessage: message,
+  };
+
+  console.log(data);
+}
+
+console.log("hellodddd");
